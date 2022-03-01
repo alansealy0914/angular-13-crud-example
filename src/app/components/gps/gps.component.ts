0d4dc3b1e9b8a01b93a133712ff8f3f19ddcbe20
alansealy <aposengineering@gmail.com>
2022-03-01 16:53:14 -0600

    added gps, gps/MQTT

--- src/app/components/gps/gps.component.ts
+++ src/app/components/gps/gps.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gps',
@@ -12,4 +12,51 @@ export class GpsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}*/
+
+
+import { Component, Input, OnInit } from '@angular/core';
+import { TutorialService } from 'src/app/services/tutorial.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tutorial } from 'src/app/models/tutorial.model';
+
+@Component({
+  selector: 'app-gps',
+  templateUrl: './gps.component.html',
+  styleUrls: ['./gps.component.css']
+})
+export class GpsComponent implements OnInit {
+
+  @Input() viewMode = false;
+
+  @Input() currentTutorial: Tutorial = {
+    title: '',
+    description: '',
+    published: false
+  };
+  
+  message = '';
+
+  constructor(
+    private tutorialService: TutorialService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    if (!this.viewMode) {
+      this.message = '';
+      this.getTutorial(this.route.snapshot.params["id"]);
+    }
+  }
+
+  getTutorial(id: string): void {
+    this.tutorialService.get(id)
+      .subscribe({
+        next: (data) => {
+          this.currentTutorial.id = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      });
+  }
 }
